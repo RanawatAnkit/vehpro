512dc1e5300a3a38ae26f25ecc4570e9276ee13f
RanawatAnkit <ranawatankit123@gmail.com>
2026-03-24 20:07:40 +0530

    add css

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const multer = require("multer");
+const AWS = require("aws-sdk");
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+// ✅ NO IAM KEYS (Learner Lab)
+AWS.config.update({
+  region: "us-east-1"
+});
+
+const s3 = new AWS.S3();
 const SECRET = "secret123";
 
 // LOGIN
@@ -28,14 +39,32 @@ app.post("/login", (req, res) => {
 });
 
 // ADD CAR (ADMIN)
-app.post("/add-car", (req, res) => {
-  const { name, price, description, image_url } = req.body;
+app.post("/add-car", upload.single("image"), async (req, res) => {
+  const { name, price, description } = req.body;
+  const file = req.file;
 
-  db.query(
-    "INSERT INTO cars (name, price, description, image_url) VALUES (?, ?, ?, ?)",
-    [name, price, description, image_url],
-    () => res.send("Car added")
-  );
+  const params = {
+    Bucket: "vehpro-images-ankit",
+    Key: Date.now() + "-" + file.originalname,
+    Body: file.buffer,
+    ContentType: file.mimetype
+  };
+
+  try {
+    const data = await s3.upload(params).promise();
+
+    const imageUrl = data.Location;
+
+    db.query(
+      "INSERT INTO cars (name, price, description, image_url) VALUES (?, ?, ?, ?)",
+      [name, price, description, imageUrl],
+      () => res.send("Car added")
+    );
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Upload failed");
+  }
 });
 
 // GET CARS
